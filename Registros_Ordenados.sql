be7b1a9cbb562612ed0b336f5564f9227fa8e614
ErwinIsmael <73193858+ErwinIsmael@users.noreply.github.com>
2020-11-27 00:04:20 -0600

    Update Registros_Ordenados.sql

---
+++ Registros_Ordenados.sql
@@ -0,0 +1,150 @@
+INSERT INTO institucion VALUES (10101, '', 'Tecnológico de Estudios Superiores de Jocotitlán', 'Jocotitlán', '5564334568', 'tesjo@gmail.com');
+INSERT INTO institucion VALUES (10110, '', 'Tecnológico de Estudios Superiores de Jilotepec', 'Jilotepec', '5564334561', 'tesji@gmail.com');
+INSERT INTO institucion VALUES (10111, '', 'Tecnológico de Estudios Superiores de Toluca', 'Toluca', '5564334562', 'testo@gmail.com');
+INSERT INTO institucion VALUES (11000, '', 'Tecnológico de Estudios Superiores de San Felipe del Progreso', 'San Felipe del Progreso', '5564334564', 'tesfe@gmail.com');
+
+
+INSERT INTO carreras VALUES ('IGE001','INGENIERIA EN GESTION EMPRESARIAL','Formar integralmente profesionales que contribuyan a la gestión de empresas e innovación de procesos; así como al diseño, implementación y desarrollo de sistemas estratégicos de negocios',
+                'Desarrollo de negocios e innovacion de procesos en las organizaciones',
+                'Desarrollar y aplicar habilidades directivas y la ingeniería en el diseño, creación, gestión, desarrollo, fortalecimiento e innovación de las organizaciones, con una orientación sistémica y sustentable para la toma de decisiones en forma efectiva',
+                'podrá desempeñar eficientemente funciones derivadas del área de producción, finanzas, recursos humanos, mercadotecnia y auditoría, asimismo podrá fungir como asesor o consultor de empresas, encargarse de la administración y gestión de empresas, públicas y privadas',
+                10101);
+               
+INSERT INTO carreras VALUES ('LA002','LICENCIATURA EN ARQUITECTURA', 'Formar profesionistas líderes con excelencia académica y humanística capaces de diseñar y construir el hábitat humano y participar en la toma de decisiones inherentes al mismo, con vocación de servicio a la sociedad, dentro de un marco de sustentabilidad',
+                '--',
+                'Realiza, en su totalidad, el proceso del proyecto arquitectónico y urbano, así como su construcción, sus aspectos teórico-prácticos  con una visión crítica  e innovadora, dentro de un marco de calidad  como satisfactor de un desarrollo comunitario y personal',
+                'profesional que puede laboral en instituciones públicas o privadas, en la micro, mediana o en grandes empresas; así mismo, desempeñarse en diversas áreas  de aplicación de proyectos arquitectónicos y Urbanos',
+                10101);
+               
+INSERT INTO carreras VALUES ('LC003','LICENCIATURA EN CONTADURIA',  'formar profesionalitas de la contaduria, capaces de diseñar, establecer,aplicar, controlar y evaluar sistemas de informacion contable, administrativa,financiera y fiscal',
+                'Fiscal financiera',
+                'Domina y aplica los principios de contabilidad generalmente aceptados, las normas y procedimientos  de auditorías nacionales e internacionales.Diseña, asesora y propone estrategias que permitan la toma de decisiones  de inversión y financiamiento, así como subestaciones eléctricas  e instalaciones de alta y baja tensión.
+                 Analiza e interpreta  y aplica las leyes fiscales, mercantiles, civiles, laborales y ambientales',
+                'profesional que puede laborar en todo tipo de organizaciones nacionales e internacionales, áreas industriales y de servicio, del sector público y privado; ocupando niveles gerenciales o directivos desarrollando sistemas de información financiera para una adecuada toma de decisiones',
+                10101);
+               
+INSERT INTO carreras VALUES ('LIE004','LICENCIATURA EN INGENIERIA ELECTROMECANICA',' Formar profesionistas de excelencia en Ingeniería Electromecánica, con actitud emprendedora, con liderazgo y capacidad de: analizar, diagnosticar, diseñar, seleccionar, instalar, administrar, mantener e innovar sistemas electromecánicos, en forma eficiente, segura y económica',
+                 'Automatizacion industrial',
+                 'Diseña, selecciona, instala y supervisa sistemas electro-neumáticos y electro-hidráulicos, sistemas de control lógico programable y control numérico.',
+                 'profesional que puede  desempeñarse en todo de industria u organizaciones: administrando, desarrollando proyectos y manteniendo los equipos en condiciones óptimos de operación',
+                 10101);
+                
+INSERT INTO carreras VALUES ('ISC005','INGENIERIA EN SISTEMAS COMPUTACIONALES', 'Formar profesionistas de manera integral con capacidad analítica, crítica, creativa y de liderazgo que aporten soluciones computacionales en las organizaciones, aplicando las tecnologías de información y de las comunicaciones',
+                 'Sistemas distribuidos',
+                 'Posee una visión empresarial y detecta áreas de oportunidad para aprender y desarrollar proyectos aplicado las tecnologías de información  y de las  comunicaciones.Desarrolla y administra sistemas de información, redes de computadoras y aplicaciones distribuidas.Analiza, desarrolla y programa modelos matemáticos, estadísticos y de simulación.',
+                 'profesional que puede laborar en todo tipo de organizaciones, administrando centros de procesamiento electrónico de datos y solucionando problemas administrativos y científicos.',
+                 10101);
+                
+INSERT INTO carreras VALUES ('II006','INGENIERIA INDUSTRIAL', 'Formar profesionales que contribuyan al desarrollo sustentable, con una visión sistemática, que responda a los retos que presentan los constantes cambios, en los sistemas de producción de bienes y servicios en el entorno global',
+                 ' sistemas de manufactura',
+                 ' Diseña, implementa, administra y mejora sistemas integrados de abastecimiento, producción y distribución de organizaciones productoras de bienes y servicios, de forma sustentable y considerando las normas nacionales e internacionales.',
+                 'profesional que puede laborar en instituciones públicas o privadas en micro, mediana o en grandes empresas; así mismo, desempeñarse en diversas áreas de aplicación de la ingeniería  industrial.',
+                 10101);
+                
+INSERT INTO carreras VALUES ('IME007','INGENIERIA MECATRONICA',' Formar profesionistas de la ingeniería con una base científica, tecnológica y humanística capaces de desarrollar, mantener e innovar sistemas, procesos y productos industriales y de servicios con un enfoque mecatrónico',
+                 'Automatizacion  en sistemas de produccion',
+                 'Proyecta, diseña, simula y construye sistemas, procesos, equipos y productos mecatrónicos.',
+                 'profesional que puede laborar en instituciones del área industrial, la investigación y el desarrollo tecnológico.',
+                 10101);
+                
+INSERT INTO carreras VALUES ('IQ008','INGENIERIA QUIMICA','Formar profesionistas en Ingeniería Química competentes para investigar, generar y aplicar el conocimiento científico y tecnológico, que le permita identificar y resolver problemas de diseño, operación, adaptación, optimización y administración en industrias químicas',
+                 'Gestion ambiental',
+                 'Diseña, selecciona, opera, optimiza y controla procesos en industrias químicas y de servicios con base en el desarrollo tecnológico de acuerdo a las normas de higiene y seguridad, de manera sustentable.',
+                 'puede integrarse con éxito en empresas públicas y privadas, en laboratorios de investigación así como en industrias extractivas, de transformación y de procesos químicos',
+                 10101);
+                
+INSERT INTO carreras VALUES ('IMT009','INGENIERIA EN MATERIALES','Formar profesionales éticos competentes con capacidad de liderazgo, actitud emprendedora y creativa para diseñar, investigar, desarrollar, controlar, innovar y solucionar problemas, en la ingeniería y tecnología de los materiales tradicionales, avanzados, biomateriales y nanomateriales ',
+                 'Desarrollo e innovacion de materiales funcionalizados',
+                 'Aplicar los conocimientos para optimizar los procesos de obtención, transformación y fabricación  de los materiales poliméricos, metálicos, cerámicos, compuestos, biomateriales y nanomateriales.',
+                 'La industria del plástico (polímeros),La industria del aluminio,La industria de la cerámica',
+                 10101);
+                
+INSERT INTO carreras VALUES ('IAD010','INGENIERIA EN ANIMACION DIGITAL','Formar profesionistas capaces de adaptar, desarrollar y aplicar recursos tecnológicos en la producción de animación digital y efectos visuales, impulsando la tecnología para responder a los desafíos de las industrias de la animación digital ',
+                 'Animacion digital y publicidad',
+                 'Implementa técnicas y procesos de producción de actualidad en las industrias de la animación digital y los efectos visuales',
+                 '--',
+                 10101);
+                
+INSERT INTO carreras VALUES ('TS011','TURISMO','El Turismo Rural aporta, al perfil del Licenciado en Turismo la oportunidad de experimentar el encuentro con las diferentes formas de vivir de las comunidades que cohabitan en un ambiente rural y además lo sensibiliza sobre el respeto y valor de su identidad cultural',
+                 '--',
+                 'Promueve el desarrollo sustentable en el turismo fomentando el respeto a la cultura y la naturaleza con un compromiso ético.',
+                 '--',
+                 10101);
+
+
+
+INSERT INTO Semestre VALUES (1001, 'Primero', '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1002, 'Segundo', '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1003, 'Tercero', '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1004, 'Cuarto',  '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1005, 'Quinto',  '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1006, 'Sexto',   '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1007, 'Septimo', '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1008, 'Octavo',  '2020-2021', 'ISC005');
+INSERT INTO Semestre VALUES (1009, 'Noveno',  '2020-2021', 'ISC005');
+
+ 
+
+INSERT INTO Semestre VALUES (1011, 'Primero', '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1022, 'Segundo', '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1033, 'Tercero', '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1044, 'Cuarto',  '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1055, 'Quinto',  '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1066, 'Sexto',   '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1077, 'Septimo', '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1088, 'Octavo',  '2020-2021', 'IME007');
+INSERT INTO Semestre VALUES (1099, 'Noveno',  '2020-2021', 'IME007');
+
+ 
+
+INSERT INTO Semestre VALUES (1111, 'Primero', '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1222, 'Segundo', '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1333, 'Tercero', '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1444, 'Cuarto',  '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1555, 'Quinto',  '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1666, 'Sexto',   '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1777, 'Septimo', '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1888, 'Octavo',  '2020-2021', 'IAD010');
+INSERT INTO Semestre VALUES (1999, 'Noveno',  '2020-2021', 'IAD010');
+
+
+INSERT INTO GRUPO VALUES (101,'A','LZ-301',1001);
+INSERT INTO GRUPO VALUES (102,'A','LZ-302',1001);
+INSERT INTO GRUPO VALUES (103,'A','LZ-303',1001);
+INSERT INTO GRUPO VALUES (104,'A','LZ-304',1001);
+INSERT INTO GRUPO VALUES (105,'A','LZ-305',1001);
+
+ 
+
+INSERT INTO GRUPO VALUES (106,'B','IC-101',1001);
+INSERT INTO GRUPO VALUES (107,'B','IC-102',1001);
+INSERT INTO GRUPO VALUES (108,'B','IC-103',1001);
+INSERT INTO GRUPO VALUES (109,'B','IC-104',1001);
+INSERT INTO GRUPO VALUES (110,'B','IC-105',1001);
+
+ 
+
+INSERT INTO GRUPO VALUES (111,'C','IM-701',1001);
+INSERT INTO GRUPO VALUES (112,'C','IM-702',1001);
+INSERT INTO GRUPO VALUES (113,'C','IM-703',1001);
+INSERT INTO GRUPO VALUES (114,'C','IM-704',1001);
+INSERT INTO GRUPO VALUES (115,'C','IM-705',1001);
+
+ 
+
+INSERT INTO GRUPO VALUES (116,'D','IF-501',1001);
+INSERT INTO GRUPO VALUES (117,'D','IF-502',1001);
+INSERT INTO GRUPO VALUES (118,'D','IF-503',1001);
+INSERT INTO GRUPO VALUES (119,'D','IF-504',1001);
+INSERT INTO GRUPO VALUES (120,'D','IF-505',1001);
+
+INSERT INTO GRUPO VALUES (121,'E','AI-601',1001);
+INSERT INTO GRUPO VALUES (122,'E','AI-602',1001);
+INSERT INTO GRUPO VALUES (123,'E','AI-603',1001);
+INSERT INTO GRUPO VALUES (124,'E','AI-604',1001);
+INSERT INTO GRUPO VALUES (125,'E','AI-605',1001);
+INSERT INTO GRUPO VALUES (126,'F','L0-201',1001);
+INSERT INTO GRUPO VALUES (127,'F','L0-202',1001);
+INSERT INTO GRUPO VALUES (128,'F','L0-203',1001);
+INSERT INTO GRUPO VALUES (129,'F','L0-204',1001);
+INSERT INTO GRUPO VALUES (130,'F','L0-205',1001);
