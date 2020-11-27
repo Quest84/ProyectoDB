8f2eab28650e7e4baf4dc5e4f95d0fb828e832ca
rogeliomonma <73140915+rogeliomonma@users.noreply.github.com>
2020-11-27 01:12:52 -0600

    Update Registros_Ordenados.sql

--- Registros_Ordenados.sql
+++ Registros_Ordenados.sql
@@ -475,3 +475,38 @@ INSERT INTO materias VALUES('INH-1029','Taller de herramientas intelectuales',5,
 INSERT INTO materias VALUES('SCD-1016','Lenjuajes y automatas II',4,2,3,5,9127,1001);
 INSERT INTO materias VALUES('SCH-1024','Taller de Administracion',4,4,2,2,9128,1777);
 INSERT INTO materias VALUES('SCD-1015','Lenjuajes y automatas I',4,4,2,2,9129,1666); 
+
+# Registros Unidades
+INSERT INTO unidades VALUES ('SCD-1003-1',1,'Arquitecturas de cómputo','SCD-1003');
+INSERT INTO unidades VALUES ('SCD-1003-2',2,'Estructura y funcionamiento de la CPU','SCD-1003');
+INSERT INTO unidades VALUES ('SCD-1003-3',3,'Componentes para ensamble de equipo de computo','SCD-1003');
+INSERT INTO unidades VALUES ('SCD-1003-4',4,'Procesamiento paralelo','SCD-1003');
+INSERT INTO unidades VALUES ('AEC-1061-1',1,'Sistemas operativos','AEC-1061');
+INSERT INTO unidades VALUES ('AEC-1061-2',2,'Administracion de procesos y hyperprocesador','AEC-1061');
+INSERT INTO unidades VALUES ('AEC-1061-3',3,'Administracion de memoria','AEC-1061');
+INSERT INTO unidades VALUES ('AEC-1061-4',4,'Administracion de Entrada-Salida','AEC-1061');
+INSERT INTO unidades VALUES ('AEC-1061-5',5,'Sistemas de archivos','AEC-1061');
+INSERT INTO unidades VALUES ('AEC-1061-6',6,'Proteccion y seguridad','AEC-1061');
+
+INSERT INTO unidades VALUES('SCC-1007-1',1,'Fundamentos de Ingeneria en Software','SCC-1007');
+INSERT INTO unidades VALUES('SCC-1010-5',5,'Introduccion a la Animacion Por Computadora','SCC-1010');
+INSERT INTO unidades VALUES('SCC-1007-2',2,'El modelo de negocio','SCC-1007');
+INSERT INTO unidades VALUES('SCC-1007-3',3,'Ingenria de Requerimentos','SCC-1007');
+INSERT INTO unidades VALUES('SCC-1007-4',4,'Modelo de Analisis','SCC-1007');
+INSERT INTO unidades VALUES('SCC-1007-5',5,'Calidad de Software','SCC-1007');
+INSERT INTO unidades VALUES('SCC-1010-1',1,'Introduccion a la Graficacion Por Computadora','SCC-1010');
+INSERT INTO unidades VALUES('SCC-1010-2',2,'Graficacion 2D','SCC-1010');
+INSERT INTO unidades VALUES('SCC-1010-3',3,'Graficacion 3D','SCC-1010');
+INSERT INTO unidades VALUES('SCC-1010-4',4,'Relleno, Iluminacion y Sombreado','SCC-1010');
+
+INSERT INTO unidades VALUES('SCA-1025-1',1,'El SGBD y el Lenguaje de Definición de Datos ','SCA-1025');
+INSERT INTO unidades VALUES('SCA-1025-2',2,'Lenguaje de Manipulación de Datos ','SCA-1025');
+INSERT INTO unidades VALUES('SCA-1025-3',3,'Administración de base de datos ','SCA-1025');
+INSERT INTO unidades VALUES('SCA-1025-4',4,'SQL Procedural ','SCA-1025');
+INSERT INTO unidades VALUES('SCA-1025-5',5,'Tecnologías de conectividad de base de datos ','SCA-1025');
+INSERT INTO unidades VALUES('AEC-1034-1',1,'Sistema de comunicación','AEC-1034');
+INSERT INTO unidades VALUES('AEC-1034-2',2,'Medios de transmisión','AEC-1034');
+INSERT INTO unidades VALUES('AEC-1034-3',3,'Modulación ','AEC-1034');
+INSERT INTO unidades VALUES('AEC-1034-4',4,'Técnicas de conmutación y multiplexación','AEC-1034');
+INSERT INTO unidades VALUES('AEC-1034-5',5,'Modelos y dispositivos de comunicación','AEC-1034');         
+
