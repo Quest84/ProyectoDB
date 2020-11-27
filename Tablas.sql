79ecce5da8a3acec2de16157103512ff0c097885
AaronCG04 <aaron.cruzgonzalez@gmail.com>
2020-11-27 01:28:40 -0600

    update

--- Tablas.sql
+++ Tablas.sql
@@ -1,4 +1,4 @@
---Tabla Institucion
+/*Tabla Institucion*/
 CREATE TABLE institucion(
 	id_escuela int NOT NULL,
 	matricula varchar(30) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE institucion(
 	correo varchar(30) NOT NULL,
 	CONSTRAINT pk_escuela PRIMARY KEY (id_escuela)
 );
---Tabla carreras
+/*Tabla carreras*/
 CREATE TABLE carreras ( 
 	clave_carrera varchar(10) NOT NULL,  
 	nombre varchar (100),
@@ -19,7 +19,7 @@ CREATE TABLE carreras (
 	id_escuela int NOT NULL, 
 	CONSTRAINT carreras_pk PRIMARY KEY (clave_carrera), 
 	CONSTRAINT carreras_fk1 FOREIGN KEY(id_escuela) REFERENCES institucion (id_escuela) ON DELETE CASCADE );
---Tabla semestre
+/*Tabla semestre*/
 CREATE TABLE semestre(
 	id_semestre int NOT NULL,
 	nombre varchar(30) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE semestre(
 	CONSTRAINT fk_carrera_semestre FOREIGN KEY (clave_carrera) REFERENCES carreras (clave_carrera),
 	CONSTRAINT pk_semestre PRIMARY KEY (id_semestre)
 );
---Tabla grupos
+/*Tabla grupos*/
 CREATE TABLE grupo (
 id_grupo INT NOT NULL,
 edificio VARCHAR(5),
@@ -37,7 +37,7 @@ id_semestre INT NOT NULL,
 CONSTRAINT pk_grupo PRIMARY KEY (id_grupo),
 CONSTRAINT fk_semestre FOREIGN KEY (id_semestre) REFERENCES semestre(id_semestre) ON DELETE CASCADE 
 );
---Tabla pago
+/*Tabla pago*/
 CREATE TABLE pagos(
 	id_pago int NOT NULL,
 	folio varchar(30) NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE pagos(
 	CONSTRAINT pk_pago PRIMARY KEY (id_pago)
 );
 
---Tabla persona
+/*Tabla persona*/
 CREATE TABLE persona(
 	id_persona int NOT NULL,
 	nombre varchar(30),
@@ -64,14 +64,14 @@ CREATE TABLE persona(
 	calle varchar(50),
 	CONSTRAINT pk_id_persona PRIMARY KEY(id_persona)
 );
---Tabla alumno
+/*Tabla alumno*/
 CREATE TABLE alumnos(
 	num_control varchar(13) NOT NULL PRIMARY KEY,
 	id_persona int,
 	constraint per_alu FOREIGN KEY (id_persona) REFERENCES persona(id_persona)
 	ON DELETE CASCADE 
 );
---Tabla docentes
+/*Tabla docentes*/
 CREATE TABLE docente (
 id_docente INT NOT NULL,
 cedula VARCHAR(8),
@@ -83,7 +83,7 @@ constraint fk_docente_persona FOREIGN KEY (id_persona) REFERENCES persona(id_per
 CONSTRAINT docente_pk PRIMARY KEY (id_docente)
 );
 
- --Tabla materias
+ /*Tabla materias*/
  CREATE TABLE materias( 
 	clave_materia VARCHAR(10) NOT NULL, 
 	nombre varchar(50), 
@@ -97,7 +97,7 @@ CONSTRAINT docente_pk PRIMARY KEY (id_docente)
 	CONSTRAINT materias_fk1 FOREIGN KEY(id_docente) REFERENCES docente(id_docente)  ON DELETE CASCADE ,
 	CONSTRAINT materias_fk2 FOREIGN KEY (id_semestre) REFERENCES semestre (id_semestre)  ON DELETE CASCADE 
 ); 
--- Tabla unidades
+/* Tabla unidades*/
 CREATE TABLE unidades(
 	id_unidad varchar(10) NOT NULL PRIMARY KEY,
 	num_unidad int,
@@ -106,7 +106,7 @@ CREATE TABLE unidades(
 	CONSTRAINT materia_unidades FOREIGN KEY(clave_materia) REFERENCES materias(clave_materia)
 	ON DELETE CASCADE 
 );
---Tabla opcion
+/*Tabla opcion*/
 CREATE TABLE opcion(
 	id_opcion int not null, 
 	nombre_opcion varchar(50) not null,
@@ -115,7 +115,7 @@ CREATE TABLE opcion(
 	CONSTRAINT id_opcion_pk primary key(id_opcion)
 );
 
---Tabla calificaciones
+/*Tabla calificaciones*/
 CREATE TABLE calificaciones(
 	num_control varchar(13) not null ,
 	id_unidad varchar(10) not null,
@@ -126,7 +126,7 @@ CREATE TABLE calificaciones(
 	CONSTRAINT opcion_calificaciones FOREIGN KEY(id_opcion) REFERENCES opcion(id_opcion)ON DELETE CASCADE 
 );
 
---Tabla preincripcion
+/*Tabla preincripcion*/
 CREATE TABLE preinscripcion (
 folio INT NOT NULL, fecha DATE, carrera_alterna1 varchar(10) NOT NULL, carrera_alterna2 varchar(10) NOT NULL, carrera_alterna3 varchar(10) NOT NULL,
 id_persona INT NOT NULL, fotografia VARCHAR(20), escuela_procedencia VARCHAR(100), municipio_escuela VARCHAR(100), promedio VARCHAR(10),
@@ -135,7 +135,7 @@ CONSTRAINT carrera1_FK FOREIGN KEY (carrera_alterna1) REFERENCES carreras (clave
 CONSTRAINT carrera2_FK FOREIGN KEY (carrera_alterna2) REFERENCES carreras(clave_carrera) ON DELETE CASCADE, 
 CONSTRAINT carrera3_FK FOREIGN KEY (carrera_alterna3) REFERENCES carreras (clave_carrera) ON DELETE CASCADE ,
 CONSTRAINT idpersona_FK FOREIGN KEY (id_persona) REFERENCES persona (id_persona) ON DELETE CASCADE);
---Tabla 
+/*Tabla */
 CREATE TABLE examen (
 id_examen INT NOT NULL 
 , horario VARCHAR(5)
@@ -146,7 +146,7 @@ id_examen INT NOT NULL
 , CONSTRAINT EXAMEN_PK PRIMARY KEY (id_examen ),
 CONSTRAINT fk_examen_pago FOREIGN KEY (folio) REFERENCES preinscripcion (folio) ON DELETE CASCADE);
 
---Tabla resultados examen
+/*Tabla resultados examen*/
 create table resultados (
 id_resultado int not null,
 resultado varchar(30),
@@ -158,7 +158,7 @@ constraint id_examen_fk foreign key(id_examen) references examen(id_examen) ON D
 constraint id_folio_fk foreign key(folio) references preinscripcion(folio) ON DELETE CASCADE 
 );
 
--- Tabla CargaAcademica
+/* Tabla CargaAcademica*/
 CREATE TABLE carga_academica(
 	id_carga_acad int NOT NULL,
 	num_control	varchar(13) NOT NULL,
@@ -168,7 +168,7 @@ CREATE TABLE carga_academica(
 	CONSTRAINT fk_grupo_carga FOREIGN KEY (id_grupo) REFERENCES grupo (id_grupo) ON DELETE CASCADE 
 );
 
--- Tabla Inscripciones
+/* Tabla Inscripciones*/
 CREATE TABLE inscripciones(
 	id_inscripcion int,
 	id_carga_acad  int,
@@ -177,7 +177,7 @@ CREATE TABLE inscripciones(
 	ON DELETE CASCADE 
 );
 
--- Tabla MateriasCarga
+/* Tabla MateriasCarga*/
 CREATE TABLE materias_carga(
 	id_carga_acad  int NOT NULL,
 	clave_materia varchar(10) NOT NULL,
@@ -191,7 +191,7 @@ CREATE TABLE inscripciones_pago(
 	CONSTRAINT fk_insc_pago FOREIGN KEY (id_inscripcion) REFERENCES inscripciones (id_inscripcion)ON DELETE CASCADE ,
 	CONSTRAINT fk_pago_insc FOREIGN KEY (id_pago) REFERENCES Pagos (id_pago)ON DELETE CASCADE 
 );
---Tabla tipo
+/*Tabla tipo*/
 create table tipo(
 id_tipo int not null,
 nombre varchar(50),
@@ -200,8 +200,8 @@ precio int,
 constraint id_tipo_pk primary key(id_tipo)
 );
 
---Tabla constancia
-create table constancia(
+/*Tabla constancia
+create table constancia(*/
 id_constancia int not null, 
 fecha date,
 num_control varchar(13) not null, 
@@ -213,7 +213,7 @@ references alumnos(num_control)ON DELETE CASCADE,
 constraint id_tipo_fk foreign key(id_tipo)references tipo(id_tipo) ON DELETE CASCADE ,
 constraint id_pago_fk foreign key(id_pago)references pagos(id_pago) ON DELETE CASCADE
 );
---tabla certificado
+/*tabla certificado*/
 CREATE TABLE certificados(
 	id_certificado varchar(20) NOT NULL PRIMARY KEY,
 	num_control varchar(13) NOT NULL,
